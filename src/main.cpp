98bd7f661f3aaa270920012a19e5728926010aff
LucasNahuu2K4 <109237808+LucasNahuu2K4@users.noreply.github.com>
2024-09-13 22:57:36 -0300

    Update main.cpp

--- src/main.cpp
+++ src/main.cpp
@@ -1,10 +1,16 @@
 #include <Arduino.h>
+#include "Macros.h"
+#include "Codigos.c"
+#include "DefinicionesVariables.c"
 
-int main(void){
-  while (1)
+
+int main(){
   
-  {
-  asm(nop);  /* code */
-  }
-  
+ PuertoSalida();
+
+EnviarDatos74HC595(Numeros[3]);
+RegistrarShifteo(Numeros[2]);
+  config_TIMER0(); // configuro el Timer0
+  sei();           // habilita las interrupciones
 }
+
